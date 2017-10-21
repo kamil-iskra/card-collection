90eedbd83e849a20823ee5ee192954be5b3f6d56
Kamil <kamil.iskra@outlook.com>
2017-10-21 19:39:27 +0200

    Add custom object in AppComponent

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -8,15 +8,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.title = 'Card collection app';
+        this.card = {
+            id: 1,
+            name: 'My card',
+            content: 'coooooontent content content'
+        };
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        template: "<h1>{{title}}</h1><h2>{{card.name}} details!</h2>"
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
+var Card = (function () {
+    function Card() {
+    }
+    return Card;
+}());
+exports.Card = Card;
 //# sourceMappingURL=app.component.js.map
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{card.name}} details!</h2>
+  <div><label>id: </label>{{card.id}}</div>
+  <div><label>name: </label>{{card.name}}</div>
+  <div><label>content: </label>{{card.content}}</div>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  title = 'Card collection app';
+  card: Card = {
+    id: 1,
+    name: 'My card',
+    content: 'coooooontent content content'
+  }; 
+}
+
+export class Card {
+  id: number;
+  name: string;
+  content: string;
+}
