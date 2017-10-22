3fe44dd7782a11d5f54ee0b74758c13dec3ae9ea
Kamil <kamil.iskra@outlook.com>
2017-10-22 20:45:44 +0200

    Add cards list and selected card event

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -9,18 +9,18 @@ var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Card collection app';
-        this.card = {
-            id: 1,
-            name: 'My card',
-            content: 'coooooontent content content'
-        };
+        this.cards = CARDS;
     }
+    AppComponent.prototype.onSelect = function (card) {
+        this.selectedCard = card;
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n  <h1>{{title}}</h1>\n  <h2>{{card.name}} details!</h2>\n  <div>\n    <label>id: </label>\n    {{card.id}}\n  </div>\n  <div>\n    <label>name: </label>\n    <input [(ngModel)]=\"card.name\" placeholder=\"name\">\n  </div>\n  <div>\n    <label>content: </label>\n    {{card.content}}\n  </div>\n  "
+        template: "\n  <h1>{{title}}</h1>\n  <h2>My Cards</h2>\n  <ul class=\"cards\">\n    <li *ngFor=\"let card of cards\" (click)=\"onSelect(card)\" [class.selected]=\"card === selectedCard\">\n      <span class=\"badge\">{{card.id}}</span> {{card.name}}\n    </li>\n  </ul>\n\n  <div *ngIf=\"selectedCard\">\n    <h2>{{selectedCard.name}} details!</h2>\n    <div>\n      <label>id: </label>\n      {{selectedCard.id}}\n    </div>\n    <div>\n      <label>name: </label>\n      <input [(ngModel)]=\"selectedCard.name\" placeholder=\"name\">\n    </div>\n    <div>\n      <label>content: </label>\n      {{selectedCard.content}}\n    </div>\n  </div>\n  ",
+        styles: ["\n  .selected {\n    background-color: #3a9fcc !important;\n    color: white;\n  }\n  .cards {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 15em;\n  }\n  .cards li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n  .cards li.selected:hover {\n    background-color: #1f5096 !important;\n    color: white;\n  }\n  .cards li:hover {\n    color: white;\n    background-color: #1f5096;\n    left: .1em;\n  }\n  .cards .text {\n    position: relative;\n    top: -3px;\n  }\n  .cards .badge {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #000000;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0 0 4px;\n  }\n"]
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
@@ -30,4 +30,17 @@ var Card = (function () {
     return Card;
 }());
 exports.Card = Card;
+// Temporary objects array
+var CARDS = [
+    { id: 11, name: 'card 1', content: 'CONTENT 1' },
+    { id: 12, name: 'card 2', content: 'CONTENT 2' },
+    { id: 13, name: 'card 3', content: 'CONTENT 3' },
+    { id: 14, name: 'card 4', content: 'CONTENT 4' },
+    { id: 15, name: 'card 5', content: 'CONTENT 5' },
+    { id: 16, name: 'card 6', content: 'CONTENT 6' },
+    { id: 17, name: 'card 7', content: 'CONTENT 7' },
+    { id: 18, name: 'card 8', content: 'CONTENT 8' },
+    { id: 19, name: 'card 9', content: 'CONTENT 9' },
+    { id: 20, name: 'card 10', content: 'CONTENT 10' }
+];
 //# sourceMappingURL=app.component.js.map
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,28 +4,88 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
-  <h2>{{card.name}} details!</h2>
-  <div>
-    <label>id: </label>
-    {{card.id}}
+  <h2>My Cards</h2>
+  <ul class="cards">
+    <li *ngFor="let card of cards" (click)="onSelect(card)" [class.selected]="card === selectedCard">
+      <span class="badge">{{card.id}}</span> {{card.name}}
+    </li>
+  </ul>
+
+  <div *ngIf="selectedCard">
+    <h2>{{selectedCard.name}} details!</h2>
+    <div>
+      <label>id: </label>
+      {{selectedCard.id}}
+    </div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="selectedCard.name" placeholder="name">
+    </div>
+    <div>
+      <label>content: </label>
+      {{selectedCard.content}}
+    </div>
   </div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="card.name" placeholder="name">
-  </div>
-  <div>
-    <label>content: </label>
-    {{card.content}}
-  </div>
-  `
+  `,
+
+  styles: [`
+  .selected {
+    background-color: #3a9fcc !important;
+    color: white;
+  }
+  .cards {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .cards li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .cards li.selected:hover {
+    background-color: #1f5096 !important;
+    color: white;
+  }
+  .cards li:hover {
+    color: white;
+    background-color: #1f5096;
+    left: .1em;
+  }
+  .cards .text {
+    position: relative;
+    top: -3px;
+  }
+  .cards .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #000000;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
-export class AppComponent  { 
+export class AppComponent {
   title = 'Card collection app';
-  card: Card = {
-    id: 1,
-    name: 'My card',
-    content: 'coooooontent content content'
-  }; 
+  selectedCard: Card;
+  cards = CARDS;
+
+  onSelect(card: Card): void {
+    this.selectedCard = card;
+  }
 }
 
 export class Card {
@@ -33,3 +93,17 @@ export class Card {
   name: string;
   content: string;
 }
+
+// Temporary objects array
+const CARDS: Card[] = [
+  { id: 11, name: 'card 1', content: 'CONTENT 1' },
+  { id: 12, name: 'card 2', content: 'CONTENT 2' },
+  { id: 13, name: 'card 3', content: 'CONTENT 3' },
+  { id: 14, name: 'card 4', content: 'CONTENT 4' },
+  { id: 15, name: 'card 5', content: 'CONTENT 5' },
+  { id: 16, name: 'card 6', content: 'CONTENT 6' },
+  { id: 17, name: 'card 7', content: 'CONTENT 7' },
+  { id: 18, name: 'card 8', content: 'CONTENT 8' },
+  { id: 19, name: 'card 9', content: 'CONTENT 9' },
+  { id: 20, name: 'card 10', content: 'CONTENT 10' }
+];
